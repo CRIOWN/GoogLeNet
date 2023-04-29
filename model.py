f211ba9630e5d600b85cc2ca8392daecc8687a85
CRIOWN <128948671+CRIOWN@users.noreply.github.com>
2023-04-29 13:07:05 +0800

    init

--- model.py
+++ model.py
@@ -7,14 +7,15 @@ class GoogLeNet(nn.Module):
     def __init__(self, num_classes=1000, aux_logits=True, init_weights=False):
         super(GoogLeNet, self).__init__()
         self.aux_logits = aux_logits
-
+        # def BasicConv2d(in_channels, out_channels, **kwargs):
         self.conv1 = BasicConv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        # ceil_mode True 向上取整 False 向下取整
         self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
 
         self.conv2 = BasicConv2d(64, 64, kernel_size=1)
         self.conv3 = BasicConv2d(64, 192, kernel_size=3, padding=1)
         self.maxpool2 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
-
+# def Inception(in_channels, ch1x1, ch3x3red, ch3x3, ch5x5red, ch5x5, pool_proj):
         self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
         self.inception3b = Inception(256, 128, 128, 192, 32, 96, 64)
         self.maxpool3 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
@@ -29,16 +30,18 @@ class GoogLeNet(nn.Module):
         self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
         self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
 
+        # 使用辅助分类器
         if self.aux_logits:
             self.aux1 = InceptionAux(512, num_classes)
             self.aux2 = InceptionAux(528, num_classes)
-
+        # 自适应平均池化下采样 （自动得到高和宽）
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.4)
         self.fc = nn.Linear(1024, num_classes)
         if init_weights:
-            self._initialize_weights()
+            self._initialize_weights()  # 初始化权重
 
+        # 正向传播
     def forward(self, x):
         # N x 3 x 224 x 224
         x = self.conv1(x)
